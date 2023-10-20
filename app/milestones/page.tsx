1dc0eb596e85a3d805ecce20fd11139713548df4
Yunwei Dai <daiyunwei1998@gmail.com>
2023-10-20 10:05:38 +0800

    remove all screenshots for further work

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+'use client'
 
-export default function milestones() {
+import React from 'react';
+import { Timeline,Card } from 'antd';
+import FFIcon from '../components/FFIcon';
+
+const { Meta } = Card;
+
+export default function Milestones() {
   return (
-    <div>
-      milestones
-    </div>
-  )
+    <Timeline
+    items={[
+      {
+        children:  <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+      >
+        <Meta title="Europe Street beat" description="www.instagram.com" />
+      </Card>,
+      },
+      {
+        children: 'Solve initial network problems 2015-09-01',
+      },
+      {
+        children: 'Technical testing 2015-09-01',
+      },
+      {
+        children: 'Network problems being solved 2015-09-01',
+      },
+    ]}
+  />
+   
+  );
 }

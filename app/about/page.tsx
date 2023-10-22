8c9fd1c474fb90381c2a4fa3f8f9db37e8148849
Yunwei Dai <daiyunwei1998@gmail.com>
2023-10-22 12:35:11 +0800

    update the about and stories page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Row, Col } from 'antd';
-import { Carousel } from 'antd';
+import { Row, Col,Statistic } from 'antd';
+import FFIcon from '../components/FFIcon';
 
 function about() {
   
@@ -17,26 +17,24 @@ const contentStyle: React.CSSProperties = {
      <div style={{ display: 'flex', alignItems: 'center' }}>
       <img style={{ width: '50%', height: 'auto'}} src="/images/about/cover.png" ></img>
       <div>
-      <h2 style={{color:'#FFAEBC'}}>抹茶貓/Matcha Neko</h2>
+      <h2 style={{color:'#FFAEBC', marginTop: '0px'}}>抹茶貓/Matcha Neko</h2>
       <h3 style={{color:'#C4C4C4'}}>High-end raider, Necromancer, Warrior of Light</h3>
-      <p>I&apos;ve met awesome people like 夜子吃椰子@紅玉海, 伊弗列姆@晨曦王座, Liasa@神意之地. They are my courage and my strength, the real treasure of my quest. This site contains my memory of being Matcha the neko. The story of me and those friends I&apos;ve met, the story of us - warriors of light. May you ever walk in the light of the crystal.</p>
+      <p style={{marginRight:'50px'}}>I arrived in Eorzea in 2020. Since then, I've been a tank main in high-end raids.This site contains my memory of being Matcha the neko. The story of me and those friends I&apos;ve met, the story of us - warriors of light. May you ever walk in the light of the crystal.</p>
+      <div>
+      <Row gutter={20}>
+        <Col span={8}>
+          <Statistic title="Ultimate Raids" value={4} prefix={<FFIcon type="HighEndDuty"/> } suffix="/ 5"/>
+        </Col>
+        <Col span={8}>
+          <Statistic title="Deep Dungeons" value={3} prefix={<FFIcon type="DD"/> } suffix="/ 3" />
+        </Col>
+      </Row>
       </div>
-    </div>
-    <div>
-    <Carousel autoplay>
-    <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel>
+      <br></br>
+      <a href="https://na.finalfantasyxiv.com/lodestone/character/40688653/" target="_blank" style={{ marginRight: '20px' }}>View Lodestone{'>'}</a>
+      <a href="https://www.fflogs.com/character/id/17514901" target="_blank" >View FFLogs{'>'}</a>
+      </div>
+      
     </div>
    </div>
   )
--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Spin } from 'antd';
 
 export default function stories() {
   return (
-    <div>
-      stories
+    <div style={{ display: 'flex'}}>
+      <div style={{marginLeft:'50px'}}>
+      <p >I&apos;ve met awesome people like 夜子吃椰子@紅玉海, 伊弗列姆@晨曦王座, Liasa@神意之地. They are my courage and my strength, the real treasure of my quest. </p>
+      <br></br>
+      <h2 style={{color:'#FFAEBC'}}><Spin />  This section is still WIP. </h2>
+      </div>
+      
+      <img style={{ width: '40%', height: 'auto'}} src="/images/stories/WIP.png"></img>
     </div>
   )
 }

6f87c458225215707ab59ec84ca41c5fd084ca1f
Yunwei Dai <daiyunwei1998@gmail.com>
2023-10-26 03:02:41 +0800

    update about page mobile

--- app/about/mobile/page.tsx
+++ app/about/mobile/page.tsx
@@ -18,29 +18,30 @@ const contentStyle: React.CSSProperties = {
 
   return (
    <main className={myFont.className}>
-    <p>this page is still WIP for mobile support</p>
-     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img style={{ width: '50%', height: 'auto'}} src="/images/about/cover.png" ></img>
+
+      
       <div>
-      <h2 style={{color:'#FFAEBC', marginTop: '0px'}}>抹茶貓/Matcha Neko</h2>
+      <img style={{ width: '100%', height: 'auto', marginBottom:'15px'}} src="/images/about/cover.webp" ></img>
+      <h2 style={{color:'#FFAEBC', marginBottom:'15px'}}>抹茶貓/Matcha Neko</h2>
       <h3 style={{color:'#C4C4C4'}}>High-end raider, Necromancer, Warrior of Light</h3>
-      <p style={{marginRight:'50px'}}>I arrived in Eorzea in 2020. Since then, I&apos;ve been a tank main in high-end raids.This site contains my memory of being Matcha the neko. The story of me and those friends I&apos;ve met, the story of us - warriors of light. May you ever walk in the light of the crystal.</p>
-      <div>
-      <Row gutter={20}>
-        <Col span={8}>
+      <Row style ={{marginBottom:'15px'}} gutter={20}>
+        <Col span={10}>
           <Statistic title="Ultimate Raids" value={4} prefix={<FFIcon type="HighEndDuty"/> } suffix="/ 5"/>
         </Col>
-        <Col span={8}>
+        <Col span={10}>
           <Statistic title="Deep Dungeons" value={3} prefix={<FFIcon type="DD"/> } suffix="/ 3" />
         </Col>
       </Row>
-      </div>
-      <br></br>
       <a href="https://na.finalfantasyxiv.com/lodestone/character/40688653/" target="_blank" style={{ marginRight: '20px' }}>View Lodestone{'>'}</a>
       <a href="https://www.fflogs.com/character/id/17514901" target="_blank" >View FFLogs{'>'}</a>
+      <p style={{marginRight:'50px'}}>I arrived in Eorzea in 2020. Since then, I&apos;ve been a tank main in high-end raids.</p>
+      <p style={{marginRight:'50px'}}>This site contains my memory of being Matcha the neko. The story of me and those friends I&apos;ve met, the story of us - warriors of light. May you ever walk in the light of the crystal.</p>
+      <div>
+     
       </div>
+         </div>
       
-    </div>
+
    </main>
   )
 }
